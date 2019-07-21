9ba455f5263e65a52f1f512d522f6b7d229a200c
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-07-20 22:53:31 -0400

    Set up express

---
+++ index.js
@@ -0,0 +1,9 @@
+require("dotenv").config(); // load the .env file content
+
+const server = require("./server.js");
+
+// uses 7777 unless PORT is set up in environment
+const port = process.env.PORT || 7777;
+server.listen(port, () => console.log(`\nServer listening on port ${port}\n`));
+
+module.exports = server;
