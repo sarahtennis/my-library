0192bb0f7cad8fda648d478df5597faa23da567f
Jeff Keilman <jkeilman93@gmail.com>
2019-10-28 18:52:52 -0400

    Add migration for friends table

---
+++ db/migrations/20191028184303_create_friends_table.js
@@ -0,0 +1,22 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("friends", tbl => {
+    tbl.increments();
+    tbl
+        .integer("target_user_id")
+        .unsigned()
+        .notNullable();
+    tbl.foreign("target_user_id").references("users.id");
+    tbl
+        .integer("friend_user_id")
+        .unsigned()
+        .notNullable();
+    tbl.foreign("friend_user_id").references("users.id");
+    tbl.timestamp("created_at").defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function(knex, Promise) {users
+    return knex.schema.dropTableIfExists("friends");
+};
+  
