b234f60cd82e901fdf5dca13dbfd83c36f4af1ac
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-11-24 22:42:37 -0500

    Create messages table migration

---
+++ db/migrations/20191124223723_create_messages_table.js
@@ -0,0 +1,25 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("messages", tbl => {
+    tbl.increments();
+    tbl
+        .integer("to_user_id")
+        .unsigned()
+        .notNullable();
+    tbl.foreign("to_user_id").references("users.id");
+    tbl
+      .integer("from_user_id")
+      .unsigned()
+      .notNullable();
+    tbl.foreign("from_user_id").references("users.id");
+    tbl
+      .string("content", 500)
+      .notNullable();
+    tbl.timestamp("created_at").defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists("messages");
+};
+  
