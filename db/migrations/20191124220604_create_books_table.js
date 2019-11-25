5ee0f5e2a9bbb1f2ce224031c1985e2c7dbee122
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-11-24 22:11:38 -0500

    Create books table migration

---
+++ db/migrations/20191124220604_create_books_table.js
@@ -0,0 +1,21 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("books", tbl => {
+    tbl.increments();
+    tbl
+        .integer("user_id")
+        .unsigned()
+        .notNullable();
+    tbl.foreign("user_id").references("users.id");
+    tbl
+    .string("isbn", 13)
+    .notNullable()
+    .unique("isbn");
+    tbl.timestamp("created_at").defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists("books");
+};
+  
